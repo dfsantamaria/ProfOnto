4323beb8e4ccd6cf4c96093da188a40ed0e242e9
dfsantamaria <santamaria@dmi.unict.it>
2019-04-18 10:52:35 +0200

    add and remove device

---
+++ ontoas/src/main/java/dmi/unict/it/ontoas/test/mainTest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dmi.unict.it.ontoas.test;
+
+import dmi.unict.it.ontoas.core.OntoASCore;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+
+/**
+ *
+ * @author Daniele Francesco Santamaria
+ */
+public class mainTest
+  {
+    public static void main(String[] args)
+      {
+        File ontoFile=new File("ontologies/main/onto-as.owl");
+        File dataFile=new File("ontologies/main/dataset.owl");
+        OntoASCore ontocore=new OntoASCore();
+        try
+          {
+            ontocore.setMainOntology(ontoFile);
+            ontocore.setDatasetOntology(dataFile);
+            ontocore.setConfiguration(new ArrayList<>(Arrays.asList("config","devices")));
+            
+            
+          } 
+        catch (OWLOntologyCreationException ex)
+          {
+            Logger.getLogger(mainTest.class.getName()).log(Level.SEVERE, null, ex);
+          }
+        System.out.println(ontocore.getMainOntology().getAxiomCount());
+        
+      }
+  }
