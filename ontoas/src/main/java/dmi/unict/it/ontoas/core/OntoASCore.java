cf8c6f9fa14e6268310d15c77336afc616d1db82
dfsantamaria <santamaria@dmi.unict.it>
2019-04-18 10:11:08 +0200

    remove axioms added

--- ontoas/src/main/java/dmi/unict/it/ontoas/core/OntoASCore.java
+++ ontoas/src/main/java/dmi/unict/it/ontoas/core/OntoASCore.java
@@ -4,9 +4,17 @@
  * and open the template in the editor.
  */
 package dmi.unict.it.ontoas.core;
-
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.util.Pair;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 
@@ -16,14 +24,22 @@ import org.semanticweb.owlapi.model.OWLOntologyCreationException;
  */
 public class OntoASCore extends OntologyCore
   {
-    private HashMap<String,OWLOntology> devices; 
+    private HashMap<String, Pair<OWLOntology,File>> devices; 
     private OWLOntology dataset;
+    private ArrayList <String> configuration;
+    
     public OntoASCore()
        {
          super();
          devices=new HashMap<>();
          dataset=null;
+         configuration=new ArrayList<>();        
        }
+    
+    public void setConfiguration(ArrayList<String> conf)
+      {
+         configuration=conf;
+      }
     
     /**
      * Sets the dataset ontology from file
@@ -55,31 +71,47 @@ public class OntoASCore extends OntologyCore
     
     /**
      * Inserts a new device
-     * @param id the device id
-     * @param device the ontology representing the device
+     * @param ontologyData the string representing the ontology data
      */
-    public void insertDevices(String id, OWLOntology device)
-    {
-      this.getDevices().put(id, device);
+    public void insertDevice(String ontologyData)
+    {         
+      String id= "dev"+ new Timestamp(new Date().getTime()).toString();
+      File file=new File(id);      
+        try
+          {
+            FileWriter fwrite=new FileWriter(file);
+            
+            fwrite.write(ontologyData);
+            OWLOntology tmp=this.getMainManager().loadOntologyFromOntologyDocument(file);       
+            this.getDevices().put(id, new Pair(tmp,file));  
+          } 
+        catch (IOException | OWLOntologyCreationException ex)
+          {
+            Logger.getLogger(OntoASCore.class.getName()).log(Level.SEVERE, null, ex);
+          }
+      
+          
     }
     
     /**
      * Returns the ontology corresponding to the given device id
      * @param id the id of the device
-     * @return the OWLOntologyy representing the device
+     * @return the ontology representing the device
      */
     public OWLOntology getDevice(String id)
     {
-       return (OWLOntology) this.getDevices().get(id);
+      return (OWLOntology) this.getDevices().get(id);
     }
     
     /**
      * Removes a given device
      * @param id the id of the device to be removed
      */
-    public void removeDevice(String id)
+    public void removePermanentDevice(String id)
     {
-       this.getDevices().remove(id);
+       Pair tmp= (Pair<OWLOntology, File>)this.getDevices().remove(id);
+       this.getMainManager().removeOntology((OWLOntology)tmp.getKey());
+       ((File)tmp.getValue()).delete();
     }
             
   }
--- ontoas/src/main/java/dmi/unict/it/ontoas/main/main.java
+++ ontoas/src/main/java/dmi/unict/it/ontoas/main/main.java
@@ -7,6 +7,8 @@ package dmi.unict.it.ontoas.main;
 
 import dmi.unict.it.ontoas.core.OntoASCore;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
@@ -26,11 +28,15 @@ public class main
           {
             ontocore.setMainOntology(ontoFile);
             ontocore.setDatasetOntology(dataFile);
+            ontocore.setConfiguration(new ArrayList<>(Arrays.asList("config","devices")));
+            
+            
           } 
         catch (OWLOntologyCreationException ex)
           {
             Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
           }
         System.out.println(ontocore.getMainOntology().getAxiomCount());
+        
       }
   }
