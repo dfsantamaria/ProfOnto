d48c85e822e9e2fa3b859721e3c7fc71e1552c56
dfsantamaria <dfsantamaria@users.noreply.github.com>
2019-04-27 23:38:54 +0200

    refactoring

--- ontoas/src/main/java/dmi/unict/it/ontoas/core/OntoASCore.java
+++ ontoas/src/main/java/dmi/unict/it/ontoas/core/OntoASCore.java
@@ -74,7 +74,7 @@ public class OntoASCore extends OntologyCore
     public Path getOntologiesDeviceConfigurationPath(){return this.getConfiguration().getPaths()[2];}
     
     /**
-     * Create the folder from the give path if it does not exist
+     * Creates the folder from the give path if it does not exist
      * @param path the path of the folder
      */
     public void createFolder(Path path)
