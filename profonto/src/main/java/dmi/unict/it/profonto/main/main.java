a72680fa873f6b9ca3e552fc1f66b896a43d3e74
dfsantamaria <santamaria@dmi.unict.it>
2019-05-16 10:55:41 +0200

    Prof-Onto

---
+++ profonto/src/main/java/dmi/unict/it/profonto/main/main.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dmi.unict.it.profonto.main;
+
+import dmi.unict.it.profonto.core.Profonto;
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import org.semanticweb.owlapi.model.OWLOntologyStorageException;
+
+/**
+ *
+ * @author Daniele Francesco Santamaria
+ */
+public class main
+  {
+    public static void main(String[] args)
+      {
+        File ontoFile=new File("ontologies/main/onto-as.owl");
+        File dataFile=new File("ontologies/main/dataset.owl");
+        Profonto ontocore=new Profonto();
+        try
+          {
+            ontocore.setMainOntology(ontoFile);
+            ontocore.setDataBehaviorOntology(dataFile);
+            //ontocore.setConfiguration(new ArrayList<>(Arrays.asList("config","devices")));
+            
+            
+          } 
+        catch (OWLOntologyCreationException | OWLOntologyStorageException ex)
+          {
+            Logger.getLogger(main.class.getName()).log(Level.SEVERE, null, ex);
+          }
+        System.out.println(ontocore.getMainOntology().getAxiomCount());
+        
+      }
+  }
+
+
+
+
+/*
+
+  try
+          {
+            OWLOntologyManager localM=OWLManager.createOWLOntologyManager();
+            localM.loadOntologyFromOntologyDocument(new File("ontologies/main/onto-as.owl"));
+            OWLOntology agent=localM.loadOntologyFromOntologyDocument(new File("ontologies/test/lightagent.owl"));
+            OWLOntology config=localM.loadOntologyFromOntologyDocument(new File("ontologies/test/alan-config.owl"));
+            OWLOntology request=localM.loadOntologyFromOntologyDocument(new File("ontologies/test/user-request.owl"));
+          
+            ontocore.addDataToDataBehavior(agent.axioms());
+            ontocore.addDataToDataBehavior(config.axioms());
+            ontocore.addDataToDataBehavior(request.axioms());
+            ontocore.getMainManager().removeOntology(request);
+          } 
+        catch (OWLOntologyCreationException ex)
+          {
+            Logger.getLogger(mainTest.class.getName()).log(Level.SEVERE, null, ex);
+          }
+                
+        
+         System.out.println("Main ontology axioms count: " +ontocore.getMainOntology().getAxiomCount());
+         System.out.println("Dataset ontology axioms count: " +ontocore.getDataBeliefOntology().getAxiomCount());
+        
+         //Testing a select query
+        String query=readQuery("ontologies/test/query.sparql");
+            
+        try
+          {
+            QueryExecution execQ = ontocore.createQuery(ontocore.getDataBeliefOntology(), query);
+            ResultSet res=ontocore.performSelectQuery(execQ);
+            System.out.println(ResultSetFormatter.asText(res));
+          } 
+        catch (OWLOntologyCreationException ex)
+          {
+            Logger.getLogger(mainTest.class.getName()).log(Level.SEVERE, null, ex);
+          }
+
+        //Testing a construct query
+        query=readQuery("ontologies/test/querycon.sparql");
+        
+          try
+          {
+            
+            QueryExecution execQ = ontocore.createQuery(ontocore.getDataBeliefOntology(), query);
+            System.out.println("Output:");
+            //ontocore.performConstructQuery(execQ).forEach(System.out::println);            
+            ontocore.addDataToDataBehavior(ontocore.performConstructQuery(execQ));
+           
+            
+          } catch (OWLOntologyCreationException | IOException ex)
+          {
+            Logger.getLogger(mainTest.class.getName()).log(Level.SEVERE, null, ex);
+          }
+
+*/
