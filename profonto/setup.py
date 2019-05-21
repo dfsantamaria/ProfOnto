2c54152b0347f11295a04b0aad54cb23aff35c82
dfsantamaria <dfsantamaria@users.noreply.github.com>
2019-05-21 17:04:33 +0200

    profeta integration

--- profonto/setup.py
+++ profonto/setup.py
@@ -1,1 +1,21 @@
+import subprocess
+import sys
+import os
+from pathlib import Path
 
+
+
+def install(package):
+    subprocess.call([sys.executable, "-m", "pip", "install", package])
+
+
+
+install('telegram')
+install('spacy')
+install('pyreadline')
+
+
+mvn="mvn clean install"
+p = subprocess.Popen(mvn, shell=True, stdout = subprocess.PIPE)
+stdout, stderr = p.communicate()
+print(stdout)
--- profonto/src/main/python/main.py
+++ profonto/src/main/python/main.py
@@ -1,32 +1,46 @@
 from py4j.java_gateway import JavaGateway
+import subprocess
+import os
+import sys
+from pathlib import Path
 
 def readOntoFile(file):
  f=open(file,"r")
  return f.read()
 
 
+p = Path(__file__).parents[3]
+os.chdir(p)
+
+#jar="java -jar target\Prof-Onto-1.0-SNAPSHOT.jar"
+#process = subprocess.Popen(jar, shell=True, stdout = subprocess.PIPE)
+#stdout, stderr = process.communicate()
+#print(stdout)
+
+
+
 profontoGateWay = JavaGateway()                   # connect to the JVM
 profonto = profontoGateWay .entry_point
 
 
-user=readOntoFile("../../../ontologies/test/alan.owl")
+user=readOntoFile("ontologies/test/alan.owl")
 value = profonto.addUser(user, "alan")  #read the user data
 print("User added with exit code:", value)
 
 
-device=readOntoFile("../../../ontologies/test/lightagent.owl")
+device=readOntoFile("ontologies/test/lightagent.owl")
 value = profonto.addDevice(device, "device")  #read the device data
 print("Device added with exit code:", value)
 
 
-config=readOntoFile("../../../ontologies/test/alan-config.owl")
+config=readOntoFile("ontologies/test/alan-config.owl")
 value = profonto.addConfiguration(config, "device", "device-Conf1", "alan")  #read the device configuration data
 print("Configuration added with exit code:", value)
 
 value=profonto.syncReasonerDataBehavior(); # sync the reasoner
 print("Data Behavior synchronized with exit code:", value)
 
-request=readOntoFile("../../../ontologies/test/user-request.owl")
+request=readOntoFile("ontologies/test/user-request.owl")
 value=profonto.acceptUserRequest(request, "http://www.dmi.unict.it/user-request.owl#alan-task-1-1-1",
                                                                                 "http://www.dmi.unict.it/ontoas/alan.owl#Alan",
                                                                "http://www.dmi.unict.it/user-request.owl#alan-task-1-1-1-exec",
