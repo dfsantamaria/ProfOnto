d102cd081551dba955ac7c1adb5e5e676182f650
dfsantamaria <dfsantamaria@users.noreply.github.com>
2019-05-21 15:15:05 +0200

    profeta integration

---
+++ profonto/src/main/python/setup.py
@@ -0,0 +1,11 @@
+import subprocess
+import sys
+def install(package):
+    subprocess.call([sys.executable, "-m", "pip", "install", package])
+
+
+install('telegram')
+install('spacy')
+install('pyreadline')
+
+subprocess.call([sys.executable, "-m","maven", "clean", "install"])
