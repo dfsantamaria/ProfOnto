343844b8d70a56dde31f3ed21b1ea6afeb79e1e5
dfsantamaria <santamaria@dmi.unict.it>
2019-05-25 11:24:49 +0200

    Setup enhanced

--- profonto/setup.py
+++ profonto/setup.py
@@ -8,16 +8,20 @@ from distutils.dir_util import copy_tree
 def install(package):
     subprocess.call([sys.executable, "-m", "pip", "install", package])
 
+print("Installing required python packages, please wait...")
 
 install('telegram')
 install('spacy')
 install('pyreadline')
 
+print("Running maven, please wait...")
 
 mvn="mvn clean install"
 p = subprocess.Popen(mvn, shell=True, stdout = subprocess.PIPE)
 stdout, stderr = p.communicate()
 print(stdout)
-
+print("Almost finished...")
+print("Copying required files...")
 copy_tree("ontologies", "target/ontologies")
 copy_tree("src/main/python", "target/python")
+print("Setup finished")
