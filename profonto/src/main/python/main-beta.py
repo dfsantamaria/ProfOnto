7010c212b88d9059da67e8634a3415f0f7100ddc
dfsantamaria <dfsantamaria@users.noreply.github.com>
2019-06-05 16:56:29 +0200

    test

--- profonto/src/main/python/main-beta.py
+++ profonto/src/main/python/main-beta.py
@@ -8,9 +8,10 @@ import os
 import time
 import socket
 
-from lib.profeta.Types import *
-from lib.profeta.Main import *
-from lib.profeta.Lib import *
+from phidias.Types import *
+from phidias.Main import *
+from phidias.Lib import *
+
 from pathlib import Path
 from threading import *
 
@@ -29,28 +30,52 @@ class client(Thread):
             self.sock.send(b'Oi you sent something to me')
 
 
-class init(Goal): pass
+def init_gateway():
+    p = Path(__file__).parents[1]
+    os.chdir(p)
+    folder = 'ontologies/devices'
+    for the_file in os.listdir(folder):
+        file_path = os.path.join(folder, the_file)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+            # elif os.path.isdir(file_path): shutil.rmtree(file_path)
+        except Exception as e:
+            print(e)
+    jar = "java -jar Prof-Onto-1.0-SNAPSHOT.jar"
+    process = subprocess.Popen(jar, universal_newlines=True, stdout=subprocess.PIPE)
+    # stdout, stderr = process.communicate()
+    welcome = ''
+    while True:
+        out = process.stdout.read(1)
+        if out != '\n':
+            welcome += out
+        else:
+            break
+    print(welcome)
+
+    profontoGateWay = JavaGateway()  # connect to the JVM
+    profonto = profontoGateWay.entry_point
+    return
 
 
-
-class init_gateway(Action):
-  def execute(self):
-      print("init ontological core")
-
-
-
-class init_server(Action):
-  def execute(self):
-      print("init server")
+def init_server():
       serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
       host = 'localhost'
       port = 8000
       serversocket.bind((host, port))
       serversocket.listen(5)
+      print("Prof-Onto Assistant has been started: send requests to:", host, "port ", port)
       while 1:
           clientsocket, address = serversocket.accept()
           client(clientsocket, address)
+      return
 
 
+class launch(Procedure): pass
 
-init() >> [init_gateway(), init_server()]
+
+launch() >>  [init_gateway(), init_server()]
+
+PHIDIAS.run()
+#PHIDIAS.shell(globals())
