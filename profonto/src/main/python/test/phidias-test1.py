edfcedd5f0996b8073c466b80d59ab83c9255baf
dfsantamaria <dfsantamaria@users.noreply.github.com>
2019-06-10 09:53:36 +0200

    test phidias

---
+++ profonto/src/main/python/test/phidias-test1.py
@@ -0,0 +1,18 @@
+from phidias.Types  import *
+from phidias.Main import *
+from phidias.Lib import *
+
+class say_hello(Procedure): pass
+class hello(Procedure): pass
+
+class Assign(Action):
+    def execute(self):
+      PHIDIAS.achieve(hello())
+
+
+say_hello() >> [Assign()]
+hello() >> [show_line("hi")]
+PHIDIAS.run()
+
+
+PHIDIAS.achieve(say_hello())
